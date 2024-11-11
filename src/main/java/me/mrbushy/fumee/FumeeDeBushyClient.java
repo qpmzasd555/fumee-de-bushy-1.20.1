117add9d7ffc0231c807b6f7bef43babc58a76b3
MrBushy <asd401696@gmail.com>
2024-11-11 14:39:36 +0300

    Initial commit

---
+++ src/main/java/me/mrbushy/fumee/FumeeDeBushyClient.java
@@ -0,0 +1,10 @@
+package me.mrbushy.fumee;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FumeeDeBushyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
